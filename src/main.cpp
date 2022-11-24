d38f2d396b5adca5514e7a3bd8cfe041849b6d1f
ENGO150 <v.smejkal06@gmail.com>
2022-11-24 19:02:42 +0100

    created & implemented coordinate typedef in rendering

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+typedef struct
+{
+    float x;
+    float y;
+} coordinate;
+
 int main(void)
 {
     GLFWwindow* window;
@@ -24,14 +30,20 @@ int main(void)
         return 1;
     }
 
+    coordinate coord1 = { -.5f, -.5f };
+    coordinate coord2 = { .5f, -.5f };
+    coordinate coord3 = { -.0f, .5f };
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
+
+        glVertex2f(coord1.x, coord1.y);
+        glVertex2f(coord2.x, coord2.y);
+        glVertex2f(coord3.x, coord3.y);
+
         glEnd();
 
         glfwSwapBuffers(window);
